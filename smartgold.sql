789001c88d73e3cd56129a2f0a260cc60793309e
jayaprasad356 <jayaprasad356@gmail.com>
2022-03-02 10:15:07 +0530

    db added

---
+++ smartgold.sql
@@ -0,0 +1,505 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 02, 2022 at 05:43 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `smartgold`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+
+CREATE TABLE `address` (
+  `id` int(10) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `landmark` varchar(255) DEFAULT NULL,
+  `area` varchar(120) NOT NULL,
+  `city` varchar(120) NOT NULL,
+  `pincode` varchar(10) NOT NULL,
+  `default_address` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `address`
+--
+
+INSERT INTO `address` (`id`, `name`, `user_id`, `address`, `landmark`, `area`, `city`, `pincode`, `default_address`) VALUES
+(1, 'prasad', 1, '26 uppukara street', '', 'sholapuram', 'Kumbakonam', '612503', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banners`
+--
+
+CREATE TABLE `banners` (
+  `id` int(10) NOT NULL,
+  `imgUrl` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `banners`
+--
+
+INSERT INTO `banners` (`id`, `imgUrl`) VALUES
+(1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhrT-e8MqX-SSxA3pJVkYRpu3gcoccsVjPdw&usqp=CAUhttps://i.pinimg.com/originals/1e/5d/27/1e5d270c627074a21966cd113c3aa3d1.jpg'),
+(2, 'https://jewelsbox.co/images/jewelsbox-banner-23-aug-2021.jpeg'),
+(3, 'https://i.pinimg.com/originals/a1/83/7b/a1837bd992681c561de4985a48884f7a.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `budget`
+--
+
+CREATE TABLE `budget` (
+  `id` int(11) NOT NULL,
+  `budget` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `budget`
+--
+
+INSERT INTO `budget` (`id`, `budget`) VALUES
+(1, 'upto 1 lakh'),
+(2, '1 lakh to 5 lakhs'),
+(3, '5 lakhs to 10 lakhs'),
+(4, 'above 10 lakhs');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `product_id` int(10) NOT NULL,
+  `quantity` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `image` text NOT NULL,
+  `status` tinyint(4) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`, `image`, `status`) VALUES
+(1, 'Ring', 'upload/images/0780-2021-12-25.jpg', 1),
+(2, 'Chain', 'upload/images/4086-2021-12-25.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `delivery`
+--
+
+CREATE TABLE `delivery` (
+  `id` int(10) NOT NULL,
+  `charges` int(3) NOT NULL,
+  `days` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `delivery`
+--
+
+INSERT INTO `delivery` (`id`, `charges`, `days`) VALUES
+(1, 70, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nickname`
+--
+
+CREATE TABLE `nickname` (
+  `id` int(10) NOT NULL,
+  `nickname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `nickname`
+--
+
+INSERT INTO `nickname` (`id`, `nickname`) VALUES
+(1, 'Reputed Shop'),
+(2, 'Popular Shop'),
+(3, 'Good');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offers`
+--
+
+CREATE TABLE `offers` (
+  `id` int(50) NOT NULL,
+  `seller_id` int(10) NOT NULL,
+  `budget_id` int(10) NOT NULL,
+  `gram_price` int(50) NOT NULL,
+  `wastage` tinyint(2) NOT NULL,
+  `max_locked` int(10) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `valid_date` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offer_lock`
+--
+
+CREATE TABLE `offer_lock` (
+  `id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `offer_id` int(10) NOT NULL,
+  `paid_amt` int(20) NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `product_id` int(10) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `buy_method` varchar(30) NOT NULL,
+  `status` varchar(200) NOT NULL,
+  `delivery_charges` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `product_id`, `quantity`, `buy_method`, `status`, `delivery_charges`) VALUES
+(1, 1, 1, 1, '2', 'received', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `seller_id` int(11) DEFAULT NULL,
+  `name` varchar(500) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `image` text NOT NULL,
+  `description` text NOT NULL,
+  `status` varchar(200) DEFAULT NULL,
+  `discounted_price` float NOT NULL,
+  `price` float NOT NULL,
+  `stock` int(5) NOT NULL,
+  `date_added` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_approved` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `seller_id`, `name`, `category_id`, `image`, `description`, `status`, `discounted_price`, `price`, `stock`, `date_added`, `is_approved`) VALUES
+(1, 1, 'Female Ring ', 1, 'upload/images/7789-2021-12-25.jpg', 'Super Ring, Specially Made for Female ', 'Not Available', 20000, 25000, 10, '2021-12-24 19:28:37', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seller`
+--
+
+CREATE TABLE `seller` (
+  `id` int(11) NOT NULL,
+  `name` text CHARACTER SET utf8 DEFAULT NULL,
+  `store_name` text CHARACTER SET utf8 DEFAULT NULL,
+  `email` text CHARACTER SET utf8 DEFAULT NULL,
+  `mobile` text DEFAULT NULL,
+  `password` text CHARACTER SET utf8 NOT NULL,
+  `store_url` text CHARACTER SET utf8 DEFAULT NULL,
+  `logo` text CHARACTER SET utf8 DEFAULT NULL,
+  `store_description` text CHARACTER SET utf8 DEFAULT NULL,
+  `street` text CHARACTER SET utf8 DEFAULT NULL,
+  `pincode` text DEFAULT NULL,
+  `city` text DEFAULT NULL,
+  `state` text CHARACTER SET utf8 DEFAULT NULL,
+  `account_number` text CHARACTER SET utf8 DEFAULT NULL,
+  `bank_ifsc_code` text CHARACTER SET utf8 DEFAULT NULL,
+  `account_name` text CHARACTER SET utf8 DEFAULT NULL,
+  `bank_name` text CHARACTER SET utf8 DEFAULT NULL,
+  `status` tinyint(2) NOT NULL DEFAULT 0,
+  `last_updated` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `national_identity_card` text CHARACTER SET utf8 DEFAULT NULL,
+  `address_proof` text CHARACTER SET utf8 DEFAULT NULL,
+  `pan_number` text CHARACTER SET utf8 DEFAULT NULL,
+  `latitude` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+  `longitude` varchar(256) CHARACTER SET utf8 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `seller`
+--
+
+INSERT INTO `seller` (`id`, `name`, `store_name`, `email`, `mobile`, `password`, `store_url`, `logo`, `store_description`, `street`, `pincode`, `city`, `state`, `account_number`, `bank_ifsc_code`, `account_name`, `bank_name`, `status`, `last_updated`, `date_created`, `national_identity_card`, `address_proof`, `pan_number`, `latitude`, `longitude`) VALUES
+(1, 'JP', 'JP Mart', 'jp@gmail.com', '9876543210', 'e807f1fcf82d132f9bb018ca6738a19f', 'https://www.apple.com/in/', '1640372913.4344.jpg', '<p>JPMart Gold Seller</p>\r\n', 'east street', '612503', 'Kumbakonam', 'Tamil Nadu', '34325325235', 'TRGRGRGG', 'jp', 'Indian Bank', 1, NULL, '2021-12-24 19:08:33', '1640372913.4378.jpg', '1640372913.44.jpg', '132342432', '0', '0'),
+(2, 'safds', 'Test Shop', 'wre@gmail.com', '4343434', 'e10adc3949ba59abbe56e057f20f883e', '34332432', '1645933635.0093.png', '', 'east street', '612503', 'kumbakonam', 'Bihar', '34324324', '33432432', 'jp', 'cub', 1, NULL, '2022-02-27 03:47:15', '1645933635.011.png', '1645933635.0127.png', '3242143432', '32432432', '4324324');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `id` int(10) NOT NULL,
+  `price` int(10) NOT NULL,
+  `days` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `price`, `days`) VALUES
+(1, 1000, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `mobile` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `mobile`, `email`) VALUES
+(1, 'Jaya Prasad', '8778624681', 'jayaprasad356@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `banners`
+--
+ALTER TABLE `banners`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `budget`
+--
+ALTER TABLE `budget`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `delivery`
+--
+ALTER TABLE `delivery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nickname`
+--
+ALTER TABLE `nickname`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `offers`
+--
+ALTER TABLE `offers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `offer_lock`
+--
+ALTER TABLE `offer_lock`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `seller`
+--
+ALTER TABLE `seller`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `banners`
+--
+ALTER TABLE `banners`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `budget`
+--
+ALTER TABLE `budget`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `delivery`
+--
+ALTER TABLE `delivery`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `nickname`
+--
+ALTER TABLE `nickname`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `offers`
+--
+ALTER TABLE `offers`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `offer_lock`
+--
+ALTER TABLE `offer_lock`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `seller`
+--
+ALTER TABLE `seller`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
