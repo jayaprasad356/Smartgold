1843d7a4c7af75744036a16ca7cbc5feebfeedeb
jayaprasad356 <jayaprasad356@gmail.com>
2022-03-28 11:20:23 +0530

    add seller register bug fixed

--- public/add-seller-form.php
+++ public/add-seller-form.php
@@ -470,7 +470,6 @@
         }
     });
 </script>
-<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
     $('input[name=mobile]').change(function() {
