807f28fa8781859349b9142a9c124f55289e59fa
jayaprasad356 <jayaprasad356@gmail.com>
2022-04-11 15:24:30 +0530

    FILTER UPDATED API UPDATED

--- api/filter_products.php
+++ api/filter_products.php
@@ -24,7 +24,7 @@ if (!isset($_POST['accesskey'])  || trim($_POST['accesskey']) != $access_key) {
     return false;
     exit();
 }
-if ($_POST['from_price_range'] != 0 & empty($_POST['from_price_range'])) {
+if (empty($_POST['from_price_range'])) {
     $response['success'] = false;
     $response['message'] = "From Price Range is Empty";
     print_r(json_encode($response));
@@ -45,107 +45,66 @@ if (empty($_POST['sort'])) {
 $from_price_range = $db->escapeString($_POST['from_price_range']);
 $to_price_range = $db->escapeString($_POST['to_price_range']);
 $sort = $db->escapeString($_POST['sort']);
-if (empty($_POST['category_id'])){
-    if ($sort == 1){
-        $sql = "SELECT *,products.id AS id,products.name AS name FROM products LEFT JOIN seller ON products.seller_id = seller.id WHERE discounted_price > $from_price_range AND discounted_price < $to_price_range ORDER BY discounted_price ASC";
-    
-    }
-    else {
-        $sql = "SELECT *,products.id AS id,products.name AS name FROM products LEFT JOIN seller ON products.seller_id = seller.id WHERE discounted_price > $from_price_range AND discounted_price < $to_price_range ORDER BY discounted_price DESC";
-    }
-       
-        $db->sql($sql);
-        $res = $db->getResult();
-        $num = $db->numRows($res);
-        if ($num >= 1) {
-            foreach ($res as $row) {
-                $temp['id'] = $row['id'];
-                $temp['seller_id'] = $row['seller_id'];
-                $temp['name'] = $row['name'];
-                $temp['store_name'] = $row['store_name'];
-            
-                $temp['category_id'] = $row['category_id'];
-               
-                // $temp['image'] = array();
-                $temp['image'] = DOMAIN_URL . $row['image'];
-                $temp['gender'] = $row['gender'];
-                $temp['weight'] = $row['weight'];
-                // $temp['image'][1] = DOMAIN_URL . $row['image'];
-                // $temp['image'][2] = DOMAIN_URL . $row['image'];
-                $temp['description'] = $row['description'];
-                $temp['discounted_price'] = $row['discounted_price'];
-                $temp['price'] = $row['price'];
-                
-                $temp1[] = $temp;
-            }
-            $response['success'] = true;
-            $response['message'] = "products Retrived Successfully";
-            $response['data'] = $temp1;
-            print_r(json_encode($response));
-        
-        }
-        else{
-            $response['success'] = false;
-            $response['message'] = "products Not Found";
-            $response['data'] = $res;
-            print_r(json_encode($response));
-        
-        }
-    
-    
+$where = '';
+$where .= "WHERE discounted_price > '$from_price_range' AND discounted_price < '$to_price_range'";
+if (isset($_POST['category_id']) && !empty($_POST['category_id'])) {
+    $category_id = $db->escapeString($_POST['category_id']);
+    $where .= "AND category_id = '$category_id'";
+}
+if (isset($_POST['gender']) && !empty($_POST['gender'])) {
+    $gender = $db->escapeString($_POST['gender']);
+    $where .= "AND gender = '$gender'";
+}
+if (isset($_POST['weight']) && !empty($_POST['weight'])) {
+    $gender = $db->escapeString($_POST['weight']);
+    $where .= "AND weight = '$gender'";
+}
+
+if ($sort == 1){
+    $sql = "SELECT *,products.id AS id,products.name AS name FROM products LEFT JOIN seller ON products.seller_id = seller.id ".$where."  ORDER BY discounted_price ASC";
 
 }
 else {
-    $category_id = $db->escapeString($_POST['category_id']);
-    
-    if ($sort == 1){
-        $sql = "SELECT *,products.id AS id,products.name AS name FROM products LEFT JOIN seller ON products.seller_id = seller.id WHERE category_id = $category_id AND discounted_price > $from_price_range AND discounted_price < $to_price_range ORDER BY discounted_price ASC";
-    
-    }
-    else {
-        $sql = "SELECT *,products.id AS id,products.name AS name FROM products LEFT JOIN seller ON products.seller_id = seller.id WHERE category_id = $category_id AND discounted_price > $from_price_range AND discounted_price < $to_price_range ORDER BY discounted_price DESC";
-    }
-       
-        $db->sql($sql);
-        $res = $db->getResult();
-        $num = $db->numRows($res);
-        if ($num >= 1) {
-            foreach ($res as $row) {
-                $temp['id'] = $row['id'];
-                $temp['seller_id'] = $row['seller_id'];
-                $temp['name'] = $row['name'];
-                $temp['store_name'] = $row['store_name'];
-            
-                $temp['category_id'] = $row['category_id'];
-               
-                // $temp['image'] = array();
-                $temp['image'] = DOMAIN_URL . $row['image'];
-                $temp['gender'] = $row['gender'];
-                $temp['weight'] = $row['weight'];
-                // $temp['image'][1] = DOMAIN_URL . $row['image'];
-                // $temp['image'][2] = DOMAIN_URL . $row['image'];
-                $temp['description'] = $row['description'];
-                $temp['discounted_price'] = $row['discounted_price'];
-                $temp['price'] = $row['price'];
-                
-                $temp1[] = $temp;
-            }
-            $response['success'] = true;
-            $response['message'] = "products Retrived Successfully";
-            $response['data'] = $temp1;
-            print_r(json_encode($response));
-        
-        }
-        else{
-            $response['success'] = false;
-            $response['message'] = "products Not Found";
-            $response['data'] = $res;
-            print_r(json_encode($response));
-        
-        }
-    
-    
+    $sql = "SELECT *,products.id AS id,products.name AS name FROM products LEFT JOIN seller ON products.seller_id = seller.id WHERE discounted_price > $from_price_range AND discounted_price < $to_price_range ORDER BY discounted_price DESC";
 }
+    $db->sql($sql);
+    $res = $db->getResult();
+    $num = $db->numRows($res);
+    if ($num >= 1) {
+        foreach ($res as $row) {
+            $temp['id'] = $row['id'];
+            $temp['seller_id'] = $row['seller_id'];
+            $temp['name'] = $row['name'];
+            $temp['store_name'] = $row['store_name'];
+        
+            $temp['category_id'] = $row['category_id'];
+           
+            // $temp['image'] = array();
+            $temp['image'] = DOMAIN_URL . $row['image'];
+            $temp['gender'] = $row['gender'];
+            $temp['weight'] = $row['weight'];
+            // $temp['image'][1] = DOMAIN_URL . $row['image'];
+            // $temp['image'][2] = DOMAIN_URL . $row['image'];
+            $temp['description'] = $row['description'];
+            $temp['discounted_price'] = $row['discounted_price'];
+            $temp['price'] = $row['price'];
+            
+            $temp1[] = $temp;
+        }
+        $response['success'] = true;
+        $response['message'] = "products Retrived Successfully";
+        $response['data'] = $temp1;
+        print_r(json_encode($response));
+    
+    }
+    else{
+        $response['success'] = false;
+        $response['message'] = "products Not Found";
+        $response['data'] = $res;
+        print_r(json_encode($response));
+    
+    }
+
 
 
 
---
+++ api/update_profile.php
@@ -0,0 +1,70 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+include_once('../includes/crud.php');
+include_once('verify-token.php');
+include_once('../includes/variables.php');
+$db = new Database();
+$db->connect();
+
+if (!verify_token()) {
+    return false;
+}
+
+if (!isset($_POST['accesskey'])  || trim($_POST['accesskey']) != $access_key) {
+    $response['success'] = false;
+    $response['message'] = "No Accsess key found!";
+    print_r(json_encode($response));
+    return false;
+    exit();
+}
+if (empty($_POST['user_id'])) {
+    $response['success'] = false;
+    $response['message'] = "User Id is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+if (empty($_POST['name'])) {
+    $response['success'] = false;
+    $response['message'] = "Name is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+if (empty($_POST['email'])) {
+    $response['success'] = false;
+    $response['message'] = "Email is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+$user_id = $db->escapeString($_POST['user_id']);
+$name = $db->escapeString($_POST['name']);
+$email = $db->escapeString($_POST['email']);
+
+$sql = "SELECT * FROM users WHERE user_id = '" . $user_id . "'";
+$db->sql($sql);
+$res = $db->getResult();
+$num = $db->numRows($res);
+if ($num == 1) {
+    $sql = "UPDATE `users` SET `name`='$name',`email`='$email' WHERE id=" . $user_id;
+    $db->sql($sql);
+    $response['success'] = true;
+    $response['message'] = "User Updated Successfully";
+
+}
+else{
+    $response['success'] = false;
+    $response['message'] = "User Not Found";
+}
+
+print_r(json_encode($response));
+
+
+
+
+?>
