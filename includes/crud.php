70812e0ae749be6a2ab4c354dac7f9f20d1cebb2
Divaharsha <divakarvan03@gmail.com>
2022-05-18 13:52:44 -0700

    crud changed

--- includes/crud.php
+++ includes/crud.php
@@ -15,9 +15,9 @@ class Database
      */
 
     private $db_host = "localhost";  // Change as required
-    private $db_user = "root";  // Change as required
-    private $db_pass = "";  // Change as required
-    private $db_name = "smartgold";    // Change as required
+    private $db_user = "u194698150_subi";  // Change as required
+    private $db_pass = "SmartGold@123";  // Change as required
+    private $db_name = "u194698150_smartgold";    // Change as required
 
     /*
      * Extra variables that are required by other function such as boolean con variable
